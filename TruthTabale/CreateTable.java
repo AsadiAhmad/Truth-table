ee95720d9b2481aecb99d417850fa2bdea173493
BlackAssassinCoder <85585962+BlackAssassinCoder@users.noreply.github.com>
2021-06-10 00:38:30 +0430

    Add files via upload

---
+++ TruthTabale/CreateTable.java
@@ -0,0 +1,84 @@
+package TruthTabale;
+
+public class CreateTable {
+
+    Calculator calculator = new Calculator ();
+
+    private void createVarRows (int numberOfVar, char[] variable) {
+        for (int counter = 0; counter < numberOfVar; counter++) {
+            System.out.print ("  ");
+            System.out.print (variable[counter]);
+            System.out.print ("  ");
+            if (counter != numberOfVar - 1) {
+                System.out.print ("|");
+            }
+        }
+    }
+
+    private void createRows (int numberOfVar) {
+        for (int counter = 0; counter < numberOfVar - 1; counter++) {
+            System.out.print ("_____");
+            System.out.print ("|");
+        }
+        System.out.println ("_____");
+    }
+
+    private void createStatementRows (int numberOfVar, char[] variable) {
+        createVarRows (numberOfVar, variable);
+        System.out.print ("| Res");
+        System.out.println ();
+    }
+
+    private boolean setTrueFalse (int currentChar, int currentColumn, int maxColumn) {
+        double index = Math.pow (2, maxColumn - currentColumn + 1);
+        double remaining = currentChar % index;
+        return !(remaining > index / 2) && remaining != 0;
+    }
+
+    public char[][] getTrueFalse (int column) {
+        int row = (int) Math.pow (2, column);
+        char[][] trueFalse = new char[row][column];
+        for (int counterColumn = 0; counterColumn < column; counterColumn++) {
+            for (int counterRow = 0; counterRow < row; counterRow++) {
+                trueFalse[counterRow][counterColumn] = calculator.convertBooleanToChar (setTrueFalse (counterRow + 1,counterColumn + 1 , column));
+            }
+        }
+        return trueFalse;
+    }
+
+    private char[][] mergeArrays (char[][] trueFalse, char[] result, int numberOfVar) {
+        int row = (int) Math.pow (2, numberOfVar);
+        int column = numberOfVar + 1;
+        char[][] finalResult = new char[row][column];
+        for (int counterRow = 0; counterRow < row; counterRow++) {
+            for (int counterColumn = 0; counterColumn < column - 1; counterColumn++) {
+                finalResult[counterRow][counterColumn] = trueFalse[counterRow][counterColumn];
+            }
+            finalResult[counterRow][column -1] = result[counterRow];
+        }
+        return finalResult;
+    }
+
+    private char[][] createValues (int numberOfVar, char[] variable, String statement) {
+        char[][] trueFalse = getTrueFalse (numberOfVar);
+        char[] result = calculator.calculateAllValue (statement, trueFalse, numberOfVar, variable);
+        return mergeArrays(trueFalse, result, numberOfVar);
+    }
+
+    private char[] arrayToArray (int numberOfVar, int currentRow, char[][] arrayChars) {
+        char[] result = new char[numberOfVar];
+        for (int counter = 0; counter < numberOfVar; counter++) {
+            result[counter] = arrayChars[currentRow][counter];
+        }
+        return result;
+    }
+
+    public void printTrueTable (int numberOfVar, char[] variable, String statement) {
+        createStatementRows (numberOfVar, variable);
+        for (int counter = 0; counter < Math.pow(2, numberOfVar); counter++) {
+            createRows (numberOfVar + 1);
+            createVarRows (numberOfVar + 1, arrayToArray (numberOfVar + 1, counter, createValues(numberOfVar, variable, statement)));
+            System.out.println ();
+        }
+    }
+}
---
+++ TruthTabale/ScanString.java
@@ -0,0 +1,77 @@
+package TruthTabale;
+
+public class ScanString {
+
+    public String foundInternalParenthesis (String statement) {
+        int startParenthesis = 0, endParenthesis = 0, result = -1;
+        statement = statement.replaceAll(" ","");
+        for (int counter = 0; counter < statement.length(); counter ++) {
+            if (statement.charAt (counter) == '(') {
+                startParenthesis = counter;
+                result = 0; // space free !
+            }
+            if (statement.charAt (counter) == ')' && result == 0) {
+                endParenthesis = counter;
+                result = 1; // space full !
+            }
+        }
+        return statement.substring (startParenthesis + 1, endParenthesis);
+    }
+
+    public Operator.operator foundOperator (String internalStatement) {
+        if (internalStatement.contains ("&&")) {
+            return Operator.operator.And;
+        }
+        else if (internalStatement.contains ("||")) {
+            return Operator.operator.Or;
+        }
+        else if (internalStatement.contains ("-->")) {
+            return Operator.operator.Condition;
+        }
+        else if (internalStatement.contains ("<->")) {
+            return Operator.operator.TowCondition;
+        }
+        else if(internalStatement.contains ("@@")) {
+            return Operator.operator.XOR;
+        }
+        else {
+            return Operator.operator.NULL;
+        }
+    }
+
+    public int foundFirstVariable (String internalStatement, int numberOfVar, char[] variable) {
+        for (int counter = 0; counter < numberOfVar; counter++) {
+            if (internalStatement.startsWith (String.valueOf(variable[counter]))) {
+                return counter + 2;
+            }
+            else if (internalStatement.startsWith ("~" + variable[counter])) {
+                return (counter * -1) - 2;
+            }
+            else if (internalStatement.startsWith ("T")) {
+                return 1;
+            }
+            else if (internalStatement.startsWith ("F")) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    public int foundLastVariable (String internalStatement, int numberOfVar, char[] variable) {
+        for (int counter = 0; counter < numberOfVar; counter++) {
+            if (internalStatement.endsWith (String.valueOf(variable[counter]))) {
+                return counter + 2;
+            }
+            else if (internalStatement.endsWith ("~" + variable[counter])) {
+                return (counter * -1) - 2;
+            }
+            else if (internalStatement.endsWith ("T")) {
+                return 1;
+            }
+            else if (internalStatement.endsWith ("F")) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+}
