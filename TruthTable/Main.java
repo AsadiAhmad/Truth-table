5e659745e65bbe4e002f115465a4f2c7022e4a5e
Ahmad <85585962+BlackAssassinCoder@users.noreply.github.com>
2021-06-13 03:59:30 -0700

    Add files via upload

---
+++ TruthTable/Main.java
@@ -0,0 +1,27 @@
+package TruthTable;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final CreateTable createTable = new CreateTable ();
+
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner (System.in);
+        System.out.println ("How many variable do you want ?");
+        int numberOfScan = myScanner.nextInt ();
+        System.out.println ("Please enter your name of variable");
+        char[] variable = new char[numberOfScan];
+        for (int counter = 0; counter < numberOfScan; counter++) {
+            variable[counter] = myScanner.next().charAt (0);
+        }
+        System.out.println ("Please enter your statement");
+        myScanner.nextLine (); // for fixing shit scanner bug
+        String statement = myScanner.nextLine ();
+        myScanner.close ();
+
+        statement = statement.replaceAll(" ","");
+        createTable.printTrueTable (numberOfScan, variable, statement);
+    }
+}
---
+++ TruthTable/Operator.java
@@ -0,0 +1,32 @@
+package TruthTable;
+
+public class Operator {
+
+    public enum operator {
+        Or, And, Condition, TowCondition, XOR, NULL
+    }
+
+    public boolean operatorNegation (boolean p) {
+        return !p;
+    }
+
+    public boolean operatorOr (boolean p, boolean q) {
+        return p || q;
+    }
+
+    public boolean operatorAnd (boolean p, boolean q) {
+        return p && q;
+    }
+
+    public boolean operatorCondition (boolean p, boolean q) {
+        return !p || q;
+    }
+
+    public boolean operatorTowCondition (boolean p, boolean q) {
+        return (!p || q) && (p || !q);
+    }
+
+    public boolean operatorXOR (boolean p , boolean q) {
+        return (p && !q) || (!p && q);
+    }
+}
